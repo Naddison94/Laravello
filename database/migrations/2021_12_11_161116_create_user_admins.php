9959b0bc185606622afc1db382da3b503a2bb24c
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-11 16:16:08 +0000

    Create user_admins migration

---
+++ database/migrations/2021_12_11_161116_create_user_admins.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAdmins extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_admins', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_admins');
+    }
+}
