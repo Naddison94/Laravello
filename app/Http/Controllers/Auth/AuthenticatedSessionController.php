7baabd597da808509322f1914e187bda27ea8919
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-11 12:07:26 +0000

    Update last_active on login/logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -32,6 +34,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        $user = User::find(Auth::id());
+        $user->last_active = Carbon::now();
+        $user->save();
+
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
@@ -43,6 +49,10 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
+        $user = User::find(Auth::id());
+        $user->last_active = Carbon::now();
+        $user->save();
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
