c2b196f3b393c74394c0034fe84b43594d1925d4
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-11 11:48:22 +0000

    Profile page card with username and image

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{ $user->name }}
+
+                    <div class="flex justify-left items-center">
+                        <div class="w-48 h-64 rounded-xl bg-gray-200 flex flex-col shadow">
+                            <img class="w-auto rounded-t-xl" src="https://avatars.githubusercontent.com/u/16485031?v=4" alt="avatar" />
+                            <div class="text-center flex flex-col p-2">
+                                <span class="text-base font-bold"> {{ $user->name }}</span>
+                                <span class="text-xs italic">Software Engineer</span>
+                            </div>
+                        </div>
+
+                        <div class="p-6">
+                            placeholder
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
