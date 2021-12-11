fbcab35393870a201ca07ebc3e03639ae3ed969e
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-11 13:06:02 +0000

    If a user has an avatar display it else display a placeholder

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,10 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
                     <div class="flex justify-left items-center">
                         <div class="w-48 h-64 rounded-xl bg-gray-200 flex flex-col shadow">
-                            <img class="w-auto rounded-t-xl" src="https://avatars.githubusercontent.com/u/16485031?v=4" alt="avatar" />
+                            <img class="w-auto rounded-t-xl" src="{{  $user->avatar ? '/' . $user->avatar : 'https://avatars.githubusercontent.com/u/16485031?v=4' }}" alt="avatar" />
                             <div class="text-center flex flex-col p-2">
                                 <span class="text-base font-bold"> {{ $user->name }}</span>
                                 <span class="text-xs italic">Software Engineer</span>
@@ -23,10 +22,9 @@
                             Last active: {{ \Carbon\Carbon::parse($user->last_active)->diffForHumans() }}
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
