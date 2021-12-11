e941d6175b37c785ad1801d7eb369bde72483978
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-11 16:03:16 +0000

    Add user upload directory to .gitignore  and update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
            'name' => 'Naddison',
            'email' => 'naddison@Laravello.co.uk',
            'password' => hash::make('kek'),
-           'avatar' => 'kek',
+           'avatar' => 'kek.jpg',
            'last_active' => Carbon::now(),
            'created_at' => Carbon::now(),
            'updated_at' => Carbon::now(),
