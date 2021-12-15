2c8ccf4b47b41e8d38d937311135586aebecd513
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-15 01:02:56 +0000

    Default recent posts to show

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -21,9 +21,9 @@
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="flex justify-center items-center">
                         <div class="p-6 bg-white border-b border-gray-200">
-                            My Posts
-                            <div x-data="{ open: false }">
-                                <button x-on:click="open = ! open">Toggle Dropdown</button>
+
+                            <div x-data="{ open: true }">
+                                <button x-on:click="open = ! open">My posts | hide</button>
                                 <div x-show="open">
                                     @foreach($posts as $post)
                                         <div class="flex justify-center">
