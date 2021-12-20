e0d798d566a1c62b62a0022f8ecec39681eac6b2
Naddison94 <42091733+Naddison94@users.noreply.github.com>
2021-12-20 21:07:45 +0000

    Detailed task table component

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers\Task;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return view('admin.task.index');
+        $tasks = Task::with('category', 'priority')->get();
+        $tasks->pending = Task::where('status_id', '=', 1)->paginate(12, ['*'] ,'pending');
+        $tasks->in_progress = Task::where('status_id', '=', 2)->paginate(12, ['*'] ,'in_progress');
+        $tasks->completed = Task::where('status_id', '=', 3)->paginate(12, ['*'] ,'completed');
+
+        return view('admin.task.index', compact('tasks'));
     }
 
     public function show()
     {
 
+    }
+
+    public function create()
+    {
+        return view('admin.task.create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request);
     }
 }
--- resources/views/admin/task/index.blade.php
+++ resources/views/admin/task/index.blade.php
@@ -6,9 +6,20 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center items-center">
-                <div class="container mx-auto">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200 ">
+                    <a class="no-underline hover:underline" href="{{ route('admin.task.index') }}"><h3 class="text-lg font-semibold">Project Management</h3></a>
+                    @include('components.admin.task.detailed-task-table')
+
+{{--                        <div class="md:col-span-2 xl:col-span-3">--}}
+{{--                            <a class="no-underline hover:underline" href="{{ route('admin.task.index') }}"><h3 class="text-lg font-semibold">Project Management</h3></a>--}}
+{{--                        </div>--}}
+{{--                    @include('components.admin.dashboard.pending-task-table')--}}
+{{--                    @include('components.admin.dashboard.in-progress-task-table')--}}
+{{--                    @include('components.admin.dashboard.completed-task-table')--}}
+{{--                    </div>--}}
+
 
                 </div>
             </div>
