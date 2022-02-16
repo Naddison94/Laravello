27417765015c5faf1269afb34a3f6e1e23ba4aa5
naddison94 <09007428nathan@gmail.com>
2022-02-16 23:05:47 +0000

    Post filter component improvements

--- resources/views/components/posts-filter-sidebar.blade.php
+++ resources/views/components/posts-filter-sidebar.blade.php
@@ -1,17 +1,19 @@
-<div class="flex-col">
+<div class="bg-white p-4 shadow-md rounded-2xl">
     <div class="pb-4 flex items-center max-w-md mx-auto rounded-full">
         <div>
-            <input type="text" name="search" class="w-full py-1 text-gray-900 rounded-full focus:outline-none" placeholder="Search post titles" value="{{ app('request')->input('search') }}">
+            <input type="text" name="search" class="w-full py-1 text-gray-900 rounded-full focus:outline-none" placeholder="search" value="{{ app('request')->input('search') }}">
         </div>
     </div>
 
-    <span class="font-medium text-gray-900">Category Filter</span>
-    <div class="space-y-6">
+    <span
+        class="font-medium text-gray-900">Category Filter
+    </span>
+    <div class="space-y-2">
         @foreach($categories as $category)
             <div class="flex items-center">
                 @php $checked = false @endphp
                 @if(request('category'))
-                    @foreach(request('category') as $key => $categoryId)
+                    @foreach(request('category') as $categoryId)
                         @if($categoryId == $category->id) @php $checked = true @endphp @endif
                     @endforeach
                 @endif
@@ -21,7 +23,7 @@
                 @else
                     <input name="category[]" value="{{ $category->id }}" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
                 @endif
-                    <label class="ml-3 min-w-0 flex-1 text-gray-500">{{ $category->title }}</label>
+                <label class="ml-3 min-w-0 flex-1 text-gray-500">{{ $category->title }}</label>
             </div>
         @endforeach
         <x-button>
