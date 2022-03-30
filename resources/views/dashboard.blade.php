671e3adaeb8c1c1c54f6dcbf9f624723554b4fd1
naddison94 <09007428nathan@gmail.com>
2022-03-30 22:13:52 +0100

    Create a post from post index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="{{ route('post.create') }}">
-                        <button>Create a post</button>
-                    </a>
-{{--                    @include('components.modal.create-post')--}}
+                    dashboard
                 </div>
             </div>
         </div>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,11 +8,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex">
-                <div class="h-screen sticky top-3">
-                    <form>
-                        @include('components.posts-filter-sidebar')
-                    </form>
+                <div>
+                    <div class="p-2 bg-white border-b border-gray-200 rounded-2xl text-center mb-3 shadow-sm">
+                        <a href="{{ route('post.create') }}">
+                            <button>
+                                Submit a post
+                            </button>
+                        </a>
+                    </div>
+                    <div class="h-screen sticky top-3">
+                        <form>
+                            @include('components.posts-filter-sidebar')
+                        </form>
+                    </div>
                 </div>
+
                 <div class="justify-center items-center">
                 @foreach($posts as $post)
                         <div>
