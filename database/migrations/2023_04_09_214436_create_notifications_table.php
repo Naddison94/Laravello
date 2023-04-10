681a9722c222f20997e772016af83cffea5181ec
Nathan Addison <42091733+Naddison94@users.noreply.github.com>
2023-04-10 16:16:40 +0100

    Create notifications migration

---
+++ database/migrations/2023_04_09_214436_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id')->primary();
+            $table->uuid('user_id');
+            $table->uuid('related_id');
+            $table->tinyInteger('type_id');
+            $table->boolean('resolved');
+            $table->timestamp('seen_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
