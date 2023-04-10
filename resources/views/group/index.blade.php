c7fd8e097397c042f99400c3a565dee420822712
Nathan Addison <42091733+Naddison94@users.noreply.github.com>
2023-04-10 16:17:14 +0100

    If group post index empty show message

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -25,11 +25,29 @@
                 </div>
 
                 <div class="justify-center items-center w-1/2">
-                    @foreach($posts as $post)
+                    @forelse($posts as $post)
                         <div>
                             @include('components.post-card-v2')
                         </div>
-                    @endforeach
+                    @empty
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div class="p-6 bg-white border-b border-gray-200 text-center">
+                                    Nothing here yet!
+
+                                    <br>
+                                    Join a group to see their posts here
+
+                                    <br>
+                                    Or.. create your own
+{{--                                    Or.. <a href="{{ route('group.create') }}">create your own</a>--}}
+                                </div>
+                                <div class="flex items-center justify-center">
+                                    <img src="{{ asset('screen.png') }}">
+                                </div>
+                            </div>
+                        </div>
+                    @endforelse
                     {{ $posts->links() }}
                 </div>
 
